effb50db3a00fd81f740d06020b8390937157c05
slopjong <git@slopjong.de>
2013-07-31 23:55:37 +0200

    Adapted to new class

--- app.php
+++ app.php
@@ -6,12 +6,6 @@
 if(!defined("APPSDIR"))
     die("Direct access is not allowed!");
 
-$app_dir = realpath(dirname(__FILE__));
-// remove the full path of the document root
-$app_dir = str_replace(ROOTDIR, "", $app_dir);
-
-$page->setActivePage(basename($app_dir));
-
 //********************************************************************
 
 $html = <<<HTML
